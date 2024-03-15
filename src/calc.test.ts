fa5f1fdd11c00afefc725be505b73b9aeeb5ff80
Nabetani <takenori@nabetani.sakura.ne.jp>
2024-03-15 10:46:09 +0900

    テスト追加

--- src/calc.test.ts
+++ src/calc.test.ts
@@ -11,3 +11,9 @@ test("sincos_14", () => {
   expect(r.cos).toBeCloseTo(0, 1e-30);
   expect(r.sin).toBeCloseTo(1, 1e-30);
 });
+
+test("sincos_1", () => {
+  const r = sincos(1);
+  expect(r.cos).toBeCloseTo(Math.cos(1), 1e-30);
+  expect(r.sin).toBeCloseTo(Math.sin(1), 1e-30);
+});
