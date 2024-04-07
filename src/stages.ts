5aa10da6da66a232e64bfc0857213ba556678d84
Nabetani <takenori@nabetani.sakura.ne.jp>
2024-04-07 09:54:25 +0900

    敵の種類追加

--- src/stages.ts
+++ src/stages.ts
@@ -5,12 +5,6 @@ export type pva = { p?: XY, v?: XY, a?: XY }
 type enemy_t = { p: XY, v: XY, r: number, vr?: number, im: number, pva?: (p: XY, ep: XY, ev: XY, tick: number) => pva }
 type stage_t = { goal: goal_t, enemies: enemy_t[] }
 
-const chase1 = (p: XY, ep: XY, ev: XY): pva => {
-  const a = p.subP(ep).subP(ev.mul(1));
-  const r = a.norm + 1e-3;
-  return { a: a.mul(30 / r) }
-};
-
 const e0 = (pos: XY): enemy_t => {
   return { p: pos, v: XY.xy(0, 0), r: 0, vr: 1, im: 0 }
 }
@@ -28,6 +22,16 @@ const e1 = (pos: XY, rad: number, a: number, f: number): enemy_t => {
   return { p: z.p!, v: XY.zero(), r: 0, vr: 1, im: 1, pva: move }
 }
 
+const e2 = (pos: XY, velo: XY, fric: number): enemy_t => {
+  return {
+    p: pos, v: velo, r: 0, vr: 1, im: 2, pva: (p: XY, ep: XY, ev: XY, dt: number): pva => {
+      const a = p.subP(ep).subP(ev.mul(fric));
+      const r = a.norm + 1e-3;
+      return { a: a.mul(30 / r) }
+    }
+  };
+}
+
 export const stages: (() => stage_t)[] = [
   /* 0 */ () => { throw "" },
 
@@ -37,14 +41,17 @@ export const stages: (() => stage_t)[] = [
     ]
   }),
   /* 2 */ () => {
-    const f = 2
     return {
-      goal: XY.rt(600, 0), enemies: [
-        e1(XY.xy(0, 0), 200, 0, f),
-        e1(XY.xy(0, 0), 200, 90, f),
-        e1(XY.xy(0, 0), 200, 180, f),
-        e1(XY.xy(0, 0), 200, 270, f),
-      ]
+      goal: XY.rt(600, 0), enemies: [...range(0, 4)].map((i) =>
+        e1(XY.xy(0, 0), 200, 90 * i, 2),
+      )
+    }
+  },
+  /* 3 */ () => {
+    return {
+      goal: XY.rt(600, 0), enemies: [...range(0, 4)].map((i) =>
+        e2(XY.ra(200, 0 + 90 * i), XY.ra(100, 90 + 90 * i), 3 ** i * 0.3),
+      )
     }
   },
 ];
