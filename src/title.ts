9229b17623fdd4e9f1805c867a2f57a440905b20
Nabetani <takenori@nabetani.sakura.ne.jp>
2024-04-21 12:50:50 +0900

    クリアしたら次のレベル

--- src/title.ts
+++ src/title.ts
@@ -5,8 +5,11 @@ import * as WS from './wstorage'
 
 const depth = {
   bg: 0,
-  textUI: 100,
+  button: 100,
+  textUI: 200,
 }
+type Rectangle = Phaser.Geom.Rectangle
+const Rectangle = Phaser.Geom.Rectangle
 
 export class Title extends BaseScene {
   constructor() {
@@ -15,24 +18,37 @@ export class Title extends BaseScene {
   preload() {
     this.load.image("title", `assets/title.webp`);
   }
-  addTextButton(x: number, y: number, w: number, depth: number,
-    fontSize: number, padding: number,
-    texts: string[]): Phaser.GameObjects.Graphics {
-    const t = this.add.text(x, y, texts[0], {
-      fontSize: fontSize,
-      padding: { x: padding, y: padding }
+  addTextButton(rc: Rectangle, depth: number, texts: string[]): Phaser.GameObjects.Graphics {
+    const fit = (t: Phaser.GameObjects.Text, w: number, h: number) => {
+      const s = Math.min(w / t.width, h / t.height)
+      t.setScale(s)
+    }
+
+    const t0 = this.add.text(rc.centerX, rc.top, texts[0], {
+      fontSize: rc.height / 2,
+      fontFamily: "sasn-serif",
+      padding: { x: 4, y: 4 }
     });
-    t.setOrigin(0.5, 0.5)
-    t.setDepth(depth + 1)
+    t0.setOrigin(0.5, 0)
+    fit(t0, rc.width, rc.height * 0.6)
+    t0.setDepth(depth + 1)
+    if (texts[1] != "") {
+      const t1 = this.add.text(rc.centerX, rc.bottom, texts[1], {
+        fontSize: rc.height / 2,
+        padding: { x: 4, y: 4 }
+      });
+      t1.setOrigin(0.5, 1)
+      fit(t1, rc.width, rc.height * 0.4)
+      t1.setDepth(depth + 1)
+    }
     const g = this.add.graphics()
     g.fillStyle(0, 0.5)
     g.lineStyle(3, 0, 1)
-    const rc = new Phaser.Geom.Rectangle(x - w / 2, y - t.height / 2, w, t.height)
     const args = [rc.left, rc.top, rc.width, rc.height] as const
     g.fillRect(...args)
     g.strokeRect(...args)
     g.setDepth(depth)
-    g.setInteractive(rc, (rc: Phaser.Geom.Rectangle, x: number, y: number): boolean => {
+    g.setInteractive(rc, (rc: Rectangle, x: number, y: number): boolean => {
       return rc.contains(x, y);
     });
     return g
@@ -41,13 +57,23 @@ export class Title extends BaseScene {
     const sr = WS.stageResults.value
     const { width, height } = this.canvas();
     const col = 4;
+    const h = 500 / 8 - 8
+    const debug = false
+    const w = width / col - 10
     for (const stage of range(1, 33)) {
-      const i = stage - 1
-      const x = ((i % col) / col + 0.5 / col) * width
-      const y = Math.floor(i / col) * 55 + height / 2
-      const depth = 1
-      if (stage <= 4 || (stage - 1 < sr.length && sr[stage - 1].score != undefined)) {
-        const t = this.addTextButton(x, y, width / 4.5, depth, 20, 10, [`Stage ${stage}`]);
+      if (debug || (stage <= 4 || (stage - 1 < sr.length && sr[stage - 1].score != undefined))) {
+        const i = stage - 1
+        const ix = i % col
+        const iy = (i - ix) / col
+        const x = width / col * (ix + 0.5) - w / 2
+        const y = height - (8 - iy) * (500 / 8)
+        const name = `Stage ${stage}`
+        const score = ((s: number | undefined): string => {
+          if (s == undefined) { return "" }
+          return `score: ${s}`
+        })(sr[stage]?.score)
+        const rc = new Rectangle(x, y, w, h)
+        const t = this.addTextButton(rc, depth.button, [name, score]);
         t.on("pointerdown", () => {
           this.scene.start('Main', { stage: stage });
         });
